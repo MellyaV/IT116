5465675efded4243dc0a8af798bae2ff02833573
MellyaV <mellyavargas@gmail.com>
2022-05-24 13:49:29 -0700

    running through the tree on js

--- domWalkthrough.js
+++ domWalkthrough.js
@@ -1,5 +1,9 @@
 //console.dir(document);
-
+//We're running through the tree. We're accessing the dom through the document.___
 console.log(document.domain);
-
 console.log(document.URL);
+
+console.log(document.title);
+console.log(document.doctype);
+
+console.log(document.head);
