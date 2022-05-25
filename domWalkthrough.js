51bebcb98fd2e7a5c4d790e00868e4dc18f50ab0
MellyaV <mellyavargas@gmail.com>
2022-05-25 13:50:23 -0700

    running though the tree structure

--- domWalkthrough.js
+++ domWalkthrough.js
@@ -1,9 +1,30 @@
 //console.dir(document);
-//We're running through the tree. We're accessing the dom through the document.___
-console.log(document.domain);
-console.log(document.URL);
+//We're running through the tree. We're accessing the dom through document.___ attribute
+//console.log(document.domain);
+//
+//console.log(document.URL);
+//
+//console.log(document.title);
+//
+//console.log(document.doctype);
+//
+//console.log(document.head);
+//
+//console.log(document.body);
+//Listed out the entire document on the console
+//console.log(document.all);
+//entered the entire document and passed onto a particualr index
+//console.log(document.all[10]);
+//
+//document.all[10].textContent = "Item Lister";
 
-console.log(document.title);
-console.log(document.doctype);
+//
+console.log(document.forms);
+console.log(document.links);
+console.log(document.images);
 
-console.log(document.head);
+//////////////////////////////////////////////////////////////
+
+//accessing the DOM with GETELEMENTBYID
+
+console.log(document.getElementById('header-title'));
