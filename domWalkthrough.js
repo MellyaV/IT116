dc1291dd369738a9afa11953606f9a4baffa287b
MellyaV <mellyavargas@gmail.com>
2022-05-31 14:09:16 -0700

    updates to dom

--- domWalkthrough.js
+++ domWalkthrough.js
@@ -19,12 +19,46 @@
 //document.all[10].textContent = "Item Lister";
 
 //
-console.log(document.forms);
-console.log(document.links);
-console.log(document.images);
+//console.log(document.forms);
+//console.log(document.links);
+//console.log(document.images);
 
 //////////////////////////////////////////////////////////////
-
 //accessing the DOM with GETELEMENTBYID
 
-console.log(document.getElementById('header-title'));
+//console.log(document.getElementById('header-title'));
+
+// Creaton of an object variable using the document function.
+//let headertitle = document.getElementById('header-title')
+
+//
+//headertitle.textcontent = "I'm Changing the Header";
+//headertitle.innerText = "I'm Changing it Again"
+
+//headertitle.style.color = 'hotpink';
+
+////////////////////////////////////////////////////////////////
+//GETELEMENTBYCLASSNAME
+
+var items = document.getElementsByClassName('list-group-item');
+
+console.log(items);
+console.log(items[1])
+
+//Styling proerty attributes through the variable
+
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'pink';
+items[1].textContent = 'Alpha Kappa Alpha';
+items[1].style.color = 'green';
+
+items[0].textContent = "Delta Sigma Theta"
+items[0].style.fontWeight = 'bold';
+items[0].style.backgroundColor = 'red';
+items[0].textContent = 'Oooop - Coleman Love';
+items[0].style.color = 'white';
+
+items[2].style.fontWeight = 'bold';
+items[2].style.backgroundColor = 'blue';
+items[2].textContent = 'Zee Phi - Zeta Phi Beta';
+items[2].style.color = 'white';
