0ba564ce63c2902ed09ef081dba46bccb515b396
MellyaV <mellyavargas@gmail.com>
2022-06-07 13:53:13 -0700

    If Conditions

---
+++ ConditionsLive.js
@@ -0,0 +1,50 @@
+console.log ("Hello World");
+
+
+let randomNumber = Math.random();
+
+console.log(randomNumber);
+
+//if (randomNumber < 0.5) {
+//
+//    console.log("Hello Mellya");
+//    console.log(randomNumberm);
+//}
+
+
+//if (randomNumber >= 0.5) {
+//
+//    console.log("randomNumber is greater than 0.5");
+//    console.log(randomNumber);
+//}
+
+//////////////////////////////////////////////////////////////////////
+//if else statement 
+
+//const dayOfWeek = 'Monday';
+
+//if (dayOfWeek === 'Monday') {
+
+//    console.log('This is Monday');
+
+//} else if (dayOfWeek === 'Thursday') {
+
+ //   console.log("This is Thursday");
+
+//} else if (dayOfWeek ===  'Saturday') {
+
+//    console.log("Hooray, today is Saturday");
+//}
+
+const age = prompt('Enter An Age To Drive')
+
+if (age < 5) {
+    console.log("You are less than 5! Hop off");
+
+} else if (age < 10) {
+    console.log("I'm gon need you to bounce");
+
+} else if (age >= 16) {
+
+    console.log("Let me drive the boat!");
+}
