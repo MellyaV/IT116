db7f864f3c3ba71e15e27526e1c2247d38688afc
MellyaV <mellyavargas@gmail.com>
2022-06-08 13:36:46 -0700

    add your comments

--- ConditionsLive.js
+++ ConditionsLive.js
@@ -36,15 +36,82 @@ console.log(randomNumber);
 //    console.log("Hooray, today is Saturday");
 //}
 
-const age = prompt('Enter An Age To Drive')
+//const age = prompt('Enter An Age To Drive')
 
-if (age < 5) {
-    console.log("You are less than 5! Hop off");
+//if (age < 5) {
+//    console.log("You are less than 5! Hop off");
 
-} else if (age < 10) {
-    console.log("I'm gon need you to bounce");
+//} else if (age < 10) {
+//    console.log("I'm gon need you to bounce");
 
-} else if (age >= 16) {
+//} else if (age >= 16) {
 
-    console.log("Let me drive the boat!");
+//   console.log("Let me drive the boat!");
+//}
+// ELSE --  All conditions in the logic have failed
+//else {
+
+//console.log("Last Resort If All Conditions Fail");
+//}
+
+//Case Statements
+// const expr = 'Papayas';
+
+// switch (age) {
+
+//     case 'Oranges':
+//         console.log("Oranges are $0.59 a pound");
+//         break;    
+//     case 'Mangoes':
+//         console.log("Hello Mangos!");
+//         break;
+//     case 'Papayas':
+//         console.log("Hello Papayas");
+//         break;
+//     default:
+//         console.log("Sorry, we are out of your asked item.");
+
+// }
+
+//const password = prompt("Please Enter A New Password");
+
+//if (password.length >= 6) {
+
+    //Nested Condition
+//    if (password.indexOf(' ') === -1) {
+
+//        console.log("Valid Password")
+//    }else {
+//        console.log("Password Can't Have Spaces")
+//    }
+
+//} else {
+
+//    console.log("Password is Too Short")
+//}
+
+
+//function singSong() {
+//    console.log("DO");
+//    console.log("RE");
+//    console.log("MI");
+//}
+
+//ingSong()
+//singSong()
+//singSong()
+
+//String Literal
+function greet(FirstName) {
+    
+    console.log(`Hey There, ${FirstName}`)
 }
+
+greet('Mellya')
+
+function greet1(FirstName, lastName) {
+
+    console.log(`Hey There, ${FirstName} ${lastName}`)
+}
+
+greet1('Jimny', 'Cricket')
